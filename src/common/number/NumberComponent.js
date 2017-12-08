b7d9d25cc16154fccb114874856ee238bf185017
PanZhangOne <zone01@live.cn>
2017-12-08 10:30:46 +0800

    update

--- src/common/number/NumberComponent.js
+++ src/common/number/NumberComponent.js
@@ -1,15 +1,28 @@
 import React, {Component} from 'react';
 
 class NumberComponent extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div style={{height: '40px', width: '40px', display: 'inline-flex', alignItems: 'center', fontSize: '15px', borderRadius: '50%', position: 'absolute', top: '8px', right: '4px', background: 'rgb(0, 188, 212)', color: '#fff', justifyContent: 'center'}}>{this.props.number}</div>
-        )
-    }
+  render() {
+    return (
+      <div style={{
+        height: '40px',
+        width: '40px',
+        display: 'inline-flex',
+        alignItems: 'center',
+        fontSize: '15px',
+        borderRadius: '50%',
+        position: 'absolute',
+        top: '8px',
+        right: '4px',
+        background: 'rgb(0, 188, 212)',
+        color: '#fff',
+        justifyContent: 'center'
+      }}>{this.props.number}</div>
+    )
+  }
 }
 
 export default NumberComponent;
--- src/common/postsitem/PostsItem.js
+++ src/common/postsitem/PostsItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Avatar from 'material-ui/Avatar';
 
 import {formatDate, setTitle} from '../../util/lib';
@@ -15,7 +15,8 @@ class PostsItem extends Component {
       <div className="Posts-Item">
         <div className="item-header">
           <div className="item-tab">{setTitle(this.props.posts.tab)}</div>
-          <div className="item-info">{this.props.posts.reply_count}/{this.props.posts.visit_count}{ ' / '+ formatDate(this.props.posts.last_reply_at)}</div>
+          <div
+            className="item-info">{this.props.posts.reply_count}/{this.props.posts.visit_count}{' / ' + formatDate(this.props.posts.last_reply_at)}</div>
         </div>
         <div className="item-content">
           <h2>{this.props.posts.title}</h2>
@@ -23,7 +24,7 @@ class PostsItem extends Component {
         </div>
         <div className="item-footer">
           <div className="user">
-            <Avatar size={22} src={this.props.posts.author.avatar_url} style={{verticalAlign: 'middle'}} />
+            <Avatar size={22} src={this.props.posts.author.avatar_url} style={{verticalAlign: 'middle'}}/>
             <span style={{verticalAlign: 'middle'}}> {this.props.posts.author.loginname}</span>
           </div>
           <div className="time">创建于：{new Date(this.props.posts.create_at).toLocaleDateString()}</div>
@@ -32,6 +33,5 @@ class PostsItem extends Component {
     )
   }
 }
-
 
 export default PostsItem;
